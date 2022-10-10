7b9db69312db502d7b201418f3dbaf06506a0691
lateepharh <pelumilateefat299@gmail.com>
2022-10-10 12:00:52 -0700

    committed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-
+import Todo from "./components/todo";
 function App() {
   return (
-    <div>Hello World</div>
+    <div>
+     <h1>My Todos</h1>
+     <Todo text="earn css"></Todo> 
+     <Todo text="Master css"></Todo> 
+     <Todo text="Explore full css course"></Todo> 
+     
+    </div>
+
   );
 }
 
